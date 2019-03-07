7c9cddfa88a7ae3dd833d8f37fed7d8ec9223f77
Brandon Kim <bkim2490@gmail.com>
2019-03-07 14:13:13 +0900

    bitstamp websocket connected, collect utc time, price, timestamp and write them to price.csv

--- src/app.js
+++ src/app.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 const appConfig = require('./config/main-config.js');
 
-appConfig.init();
+appConfig.init(app, express);
 
 module.exports = app;
--- src/server.js
+++ src/server.js
@@ -1,11 +1,36 @@
 const app = require('./app');
 const http = require('http');
 const server = http.createServer(app);
+const Pusher = require('pusher-js/node');
+const createCsvWriter = require('csv-writer').createObjectCsvWriter;
+const moment = require('moment');
+const csvWriter = createCsvWriter({
+    path: './price.csv',
+    header: [
+        {id: 'utc', title: 'UTC TIME'},
+        {id: 'price', title: 'PRICE'},
+        {id: 'timestamp', title: 'TIMESTAMP'}
+    ]
+});
+
+let pusher = new Pusher('de504dc5763aeef9ff52');
+
+let channel = pusher.subscribe('live_trades');
+channel.bind('trade', (data) => {
+    console.log(typeof data.timestamp)
+    csvWriter
+        .writeRecords([
+            {'utc': moment.utc(data.timestamp * 1000).format('MMM Do, h:mm:ss a'), 'price': data.price_str, 'timestamp': data.timestamp}
+        ])
+        .then(() => {
+            console.log('csv file was written');
+        })
+})
 
 server.listen(3000);
 
 app.use('/', (req, res, next) => {
-    res.send('Welcome to bitHolla');
+    res.render('static/index');
 })
 
 server.on('listening', () => {

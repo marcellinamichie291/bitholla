2a0ea6c310fd3bccbcb05d62b83953bfa11395e5
Brandon Kim <bkim2490@gmail.com>
2019-03-12 21:50:54 +0900

    bitmex and bitstamp websockets connect when user signs in and disconnect when user signs out

--- src/db/queries.prices.js
+++ src/db/queries.prices.js
@@ -2,13 +2,8 @@ const WebSocket = require('ws');
 const moment = require('moment');
 const Prices = require('../db/models').Prices;
 
-// // initialize binance websocket
-// const binance = require('node-binance-api')();
 let binanceWs;
-
-// // initialize bitmex websocket
-// const BitMEXClient = require('bitmex-realtime-api');
-// const client = new BitMEXClient();
+let bitmexWs;
 
 // initalize pusher websocket
 const Pusher = require('pusher-js/node');
@@ -16,30 +11,11 @@ const pusher = new Pusher('de504dc5763aeef9ff52');
 
 module.exports = {
     connect(user) {
-        // // listen to binance websocket for trade events
-        // binance.websockets.trades('BTCUSDT', (data) => {
-        //     let {e:eventType, E:eventTime, s:symbol, p:price, q:quantity, m:maker, a:tradeId} = data;
-        //     Prices.create({
-        //         exchange: 'Binance',
-        //         utc: moment.utc(data.E * 1000).format('MMM Do, h:mm:ss a'),
-        //         price: parseFloat(data.p).toFixed(2),
-        //         timestamp: data.E,
-        //         userId: user.id
-        //     })
-        //     .then((price) => {
-        //         console.log('binance trade recorded');
-        //     })
-        //     .catch((err) => {
-        //         console.log(err);
-        //     })
-        // })
-
+        // binance websocket connected and listening to trades
         binanceWs = new WebSocket('wss://stream.binance.com:9443/ws/btcusdt@aggTrade');
-
         binanceWs.on('open', function open() {
             console.log('binance websocket connected');
         })
-
         binanceWs.on('message', (data) => {
             data = JSON.parse(data);
             Prices.create({
@@ -57,43 +33,60 @@ module.exports = {
             })
         })
 
-        // // listen to bitmex websocket for trade events
-        // client.addStream('XBTUSD', 'trade', (data) => {
-        //     Prices.create({
-        //         exchange: 'BitMEX',
-        //         utc: moment(data[data.length - 1].timestamp).format('MMM Do, h:mm:ss a'),
-        //         price: data[data.length - 1].price.toFixed(2),
-        //         timestamp: moment(data[data.length - 1].timestamp).valueOf(),
-        //         userId: user.id
-        //     })
-        //     .then((price) => {
-        //         console.log('bitmex trade recorded');
-        //     })
-        //     .catch((err) => {
-        //         console.log(err);
-        //     })
-        // })
+        // bitmex websocket connected and listening to trades
+        bitmexWs = new WebSocket('wss://www.bitmex.com/realtime?subscribe=trade:XBTUSD');
+        bitmexWs.on('open', function open() {
+            console.log('bitmex websocket connected');
+        })
+        bitmexWs.on('message', (data) => {
+            data = JSON.parse(data);
+            if (data.data) {
+                Prices.create({
+                    exchange: 'BitMEX',
+                    utc: moment(data.data[0].timestamp).format('MMM Do, h:mm:ss a'),
+                    price: data.data[0].price.toFixed(2),
+                    timestamp: moment(data.data[0].timestamp).valueOf(),
+                    userId: user.id
+                })
+                .then((price) => {
+                    console.log('bitmex trade recorded');
+                })
+                .catch((err) => {
+                    console.log(err);
+                })
+            }
+        })
 
-        // // listen to bistamp websocket for trade events
-        // const channel = pusher.subscribe('live_trades');
-        // channel.bind('trade', (data) => {
-        //     Prices.create({
-        //         exchange: 'Bitstamp',
-        //         utc: moment.utc(data.timestamp * 1000).format('MMM Do, h:mm:ss a'),
-        //         price: parseFloat(data.price_str).toFixed(2),
-        //         timestamp: data.timestamp,
-        //         userId: user.id
-        //     })
-        //     .then((price) => {
-        //         console.log('bitstamp trade recorded');
-        //     })
-        //     .catch((err) => {
-        //         console.log(err);
-        //     })
-        // })
+        // listen to bistamp websocket for trade events
+        const channel = pusher.subscribe('live_trades');
+        channel.bind('trade', (data) => {
+            Prices.create({
+                exchange: 'Bitstamp',
+                utc: moment.utc(data.timestamp * 1000).format('MMM Do, h:mm:ss a'),
+                price: parseFloat(data.price_str).toFixed(2),
+                timestamp: data.timestamp,
+                userId: user.id
+            })
+            .then((price) => {
+                console.log('bitstamp trade recorded');
+            })
+            .catch((err) => {
+                console.log(err);
+            })
+        })
     },
 
     disconnect() {
         binanceWs.close();
+        binanceWs.on('close', function close() {
+            console.log('binance websocket closed');
+        })
+
+        bitmexWs.close();
+        bitmexWs.on('close', function close() {
+            console.log('bitmex websocket closed');
+        })
+
+        pusher.unsubscribe('live_trades');
     }
 }

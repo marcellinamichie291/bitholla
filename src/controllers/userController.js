a4aa66320ce19201cb4276180f04dbe744587928
Brandon Kim <bkim2490@gmail.com>
2019-03-13 09:03:07 +0900

    price.csv file can be downloaded by signed in user

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,6 +1,9 @@
 const userQueries = require('../db/queries.user.js');
 const pricesQueries = require('../db/queries.prices.js');
 const passport = require('passport');
+const Prices = require('../db/models').Prices;
+const Json2csvParser = require('json2csv').Parser;
+const fields = ['exchange', 'UTC time', 'price', 'timestamp'];
 
 module.exports = {
     create(req, res, next) {
@@ -42,5 +45,108 @@ module.exports = {
         req.logout();
         req.flash('notice', 'You\'ve successfully signed out!');
         res.redirect('/');
+    },
+
+    download(req, res, next) {
+        Prices.findAll({
+            where: {
+                userId: req.user.id
+            }
+        })
+        .then((prices) => {
+            const json2csvParser = new Json2csvParser({ fields });
+            let priceData = [];
+            prices.forEach((price) => {
+                priceData.push({
+                    'exchange': price.exchange,
+                    'UTC time': price.utc,
+                    'price': price.price,
+                    'timestamp': price.timestamp
+                })
+            })
+
+            const csv = json2csvParser.parse(priceData);
+            res.setHeader('Content-disposition', 'attachment; filename=price.csv');
+            res.set('Content-Type', 'text/csv');
+            res.status(200).send(csv);
+        })
+    },
+
+    downloadBinance(req, res, next) {
+        Prices.findAll({
+            where: {
+                userId: req.user.id,
+                exchange: 'Binance'
+            }
+        })
+        .then((prices) => {
+            const json2csvParser = new Json2csvParser({ fields });
+            let priceData = [];
+            prices.forEach((price) => {
+                priceData.push({
+                    'exchange': price.exchange,
+                    'UTC time': price.utc,
+                    'price': price.price,
+                    'timestamp': price.timestamp
+                })
+            })
+
+            const csv = json2csvParser.parse(priceData);
+            res.setHeader('Content-disposition', 'attachment; filename=price.csv');
+            res.set('Content-Type', 'text/csv');
+            res.status(200).send(csv);
+        })
+    },
+
+    downloadBitmex(req, res, next) {
+        Prices.findAll({
+            where: {
+                userId: req.user.id,
+                exchange: 'BitMEX'
+            }
+        })
+        .then((prices) => {
+            const json2csvParser = new Json2csvParser({ fields });
+            let priceData = [];
+            prices.forEach((price) => {
+                priceData.push({
+                    'exchange': price.exchange,
+                    'UTC time': price.utc,
+                    'price': price.price,
+                    'timestamp': price.timestamp
+                })
+            })
+
+            const csv = json2csvParser.parse(priceData);
+            res.setHeader('Content-disposition', 'attachment; filename=price.csv');
+            res.set('Content-Type', 'text/csv');
+            res.status(200).send(csv);
+        })
+    },
+
+    downloadBitstamp(req, res, next) {
+        Prices.findAll({
+            where: {
+                userId: req.user.id,
+                exchange: 'Bitstamp'
+            }
+        })
+        .then((prices) => {
+            const json2csvParser = new Json2csvParser({ fields });
+            let priceData = [];
+            prices.forEach((price) => {
+                priceData.push({
+                    'exchange': price.exchange,
+                    'UTC time': price.utc,
+                    'price': price.price,
+                    'timestamp': price.timestamp
+                })
+            })
+
+            const csv = json2csvParser.parse(priceData);
+            res.setHeader('Content-disposition', 'attachment; filename=price.csv');
+            res.set('Content-Type', 'text/csv');
+            res.status(200).send(csv);
+        })
     }
 }
--- src/routes/user.js
+++ src/routes/user.js
@@ -6,5 +6,9 @@ const validation = require('./validation');
 router.post('/users', validation.validateUsers, userController.create);
 router.post('/users/signin', userController.signin);
 router.get('/users/signout', userController.signout);
+router.get('/users/download', userController.download);
+router.get('/users/download/binance', userController.downloadBinance);
+router.get('/users/download/bitmex', userController.downloadBitmex);
+router.get('/users/download/bitstamp', userController.downloadBitstamp);
 
 module.exports = router;
